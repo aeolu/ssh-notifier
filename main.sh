23ac74647e9e6579443ed290c7f00f2695210ad2
aeolu <jaggygauran@gmail.com>
2012-07-20 00:18:15 +0800

    detection of the user action

--- main.sh
+++ main.sh
@@ -2,13 +2,39 @@
 
 <?php
 
+$exec = shell_exec("w -h"); // get all connections
+echo print_r(filterData($exec), true);
+
+
+function filterData($data){
+
+	$connections = array();
+	$headers = getConnectionHeaders();
+    $data = array_filter(explode("\n", $data));
+
+    foreach($data as $datum){
+		$temp = array();
+    	$values = preg_split("/\s+/", $datum);
+		for($i = 0; $i < count($values); $i++){
+			// Appends the excess to the job header
+			if($headers[$i])
+				$temp[$headers[$i]] = $values[$i];
+			else
+				$temp[$headers[count($headers)-1]] .= " " . $values[$i];
+		}
+		$connections[] = $temp;
+	}
+
+    return $connections;
+}
 
 function detectSystem(){
 	$operating_systems = array('darwin', 'linux', 'win');
-	$uname = strtolower(php_uname());
+	$uname = strtolower(php_uname()); // get system information
 	$system = '';
 
 	foreach($operating_systems as $operating_system){
+		// find a certain OS identifier
 		if(strpos($uname, $operating_system) !== false)
 			$system = $operating_system;
 			break;
@@ -22,7 +48,7 @@ function getConnectionHeaders(){
 	$exec = shell_exec('w');
 	
 	$row_headers = explode("\n", $exec);
-	$headers = preg_split("/\s+/", $row_headers[1]);
+	$headers = preg_split("/\s+/", $row_headers[1]); // explode by indefinite amount of whitespaces
 
 	return $headers;	
 }
