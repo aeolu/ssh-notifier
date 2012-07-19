a6ee292ffe2bda6b69488b9b201806899e843fd0
aeolu <jaggygauran@gmail.com>
2012-07-20 00:01:36 +0800

    OS detection for future-proofing

--- main.sh
+++ main.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/php
 
 <?php
+
+
+function detectSystem(){
+	$operating_systems = array('darwin', 'linux', 'win');
+	$uname = strtolower(php_uname());
+	$system = '';
+
+	foreach($operating_systems as $operating_system){
+		if(strpos($uname, $operating_system) !== false)
+			$system = $operating_system;
+			break;
+	}
+
+	return ($system) ? ucfirst($system) : 'Unkown';
+}
