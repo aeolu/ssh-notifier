e0f64c51d84d09b29afabb3ffe51a3411fc14350
aeolu <jaggygauran@gmail.com>
2012-07-20 23:55:01 +0800

    added a memory usage functionality

--- benchmark.php
+++ benchmark.php
@@ -3,9 +3,13 @@
 class Benchmark{
 	
 	private static $instance;
+	private $memory = array(
+		'current' 	=>	0,
+		'peak'		=>	0,
+	);
 
 	protected function __construct(){
-		echo "Using Benchmark class\n";
+		$this->memoryUsage();
 	}
 
 	public static function getInstance(){
@@ -13,5 +17,12 @@ class Benchmark{
 		return self::$instance;
 	}
 
-
+	private function memoryUsage(){
+		$this->memory['current'] = memory_get_usage(1)/1024;
+		$this->memory['peak'] = memory_get_peak_usage(1)/1024;
+		
+		echo "-- Memory --\n\n";
+		echo "CURRENT:\t\t{$this->memory['current']}kB\n";
+		echo "PEAK:\t\t\t{$this->memory['peak']}kB\n";
+	}
 }
