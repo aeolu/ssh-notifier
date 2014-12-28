4b0b45ff3a35abf68156cf76dcecba670d8b2014
Jaggy Gauran <jaggygauran@gmail.com>
2014-12-28 10:36:33 +0800

    inject dependencies

--- src/Jaggy/Watcher/Shell.php
+++ src/Jaggy/Watcher/Shell.php
@@ -1,21 +1,21 @@
 <?php
 namespace Jaggy\Watcher;
 
-use AdamBrett\ShellWrapper\Runners\Exec;
+use AdamBrett\ShellWrapper\Runners\ShellExec;
 use AdamBrett\ShellWrapper\Command\Builder as CommandBuilder;
 
 /**
  * Handles all the shell commands.
  *
- * @property    \AdamBrett\ShellWrapper\Runners\Exec     shell
- * @property    \AdamBrett\ShellWrapper\Command\Builder  builder
+ * @property    \AdamBrett\ShellWrapper\Command\Builder    builder
+ * @property    \AdamBrett\ShellWrapper\Runners\ShellExec  shell
  *
- * @uses        \AdamBrett\ShellWrapper\Runners\Exec
  * @uses        \AdamBrett\ShellWrapper\Command\Builder
+ * @uses        \AdamBrett\ShellWrapper\Runners\ShellExec
  *
  * @package     Jaggy\Watcher
  * @author      Jaggy Gauran <jaggygauran@gmail.com>
- * @version     Release: 0.1.1
+ * @version     Release: 0.1.2
  * @link        https://github.com/
  * @license     http://www.wtfpl.net/ Do What The Fuck You Want To Public License
  * @since       Class available since Release 1.1.0
@@ -24,14 +24,29 @@ class Shell
 {
 
     /**
+     * @access protected
+     * @var    \AdamBrett\ShellWrapper\Command\Builder
+     */
+    protected $builder;
+
+    /**
+     * @access protected
+     * @var    \AdamBrett\ShellWrapper\Runners\ShellExec
+     */
+    protected $shell;
+
+
+    /**
      * Inject dependencies
      *
-     * @param  \AdamBrett\ShellWrapper\Runners\Exec    $shell
-     * @param  \AdamBrett\ShellWrapper\Command\Builder $builder
+     * @param  \AdamBrett\ShellWrapper\Runners\ShellExec $shell
+     * @param  \AdamBrett\ShellWrapper\Command\Builder   $builder
      *
      * @access public
      */
-    public function __construct(Exec $shell, CommandBuilder $builder)
+    public function __construct(ShellExec $shell, CommandBuilder $builder)
     {
+        $this->shell   = $shell;
+        $this->builder = $builder;
     }
 }
--- tests/ShellTest.php
+++ tests/ShellTest.php
@@ -7,7 +7,7 @@ use Jaggy\Watcher\Shell;
  * @uses        \Jaggy\Watcher\Shell
  *
  * @author      Jaggy Gauran <jaggygauran@gmail.com>
- * @version     Release: 0.1.0
+ * @version     Release: 0.1.1
  * @link        https://github.com/
  * @license     http://www.wtfpl.net/ Do What The Fuck You Want To Public License
  * @since       Class available since Release 0.1.0
@@ -24,6 +24,12 @@ class ShellTest extends PHPUnit_Framework_TestCase
      */
     public function it_is_initializable()
     {
+        $exec    = Mockery::mock('AdamBrett\ShellWrapper\Runners\ShellExec');
+        $builder = Mockery::mock('AdamBrett\ShellWrapper\Command\Builder');
+
+        $shell = new Shell($exec, $builder);
+
+        $this->assertInstanceOf('Jaggy\Watcher\Shell', $shell);
     }
 
 
