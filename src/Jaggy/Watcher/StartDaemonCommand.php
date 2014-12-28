1d95832efc7174857236dbfef668d0ed14843916
Jaggy Gauran <jaggygauran@gmail.com>
2014-12-28 09:51:59 +0800

    refactor out command to class

---
+++ src/Jaggy/Watcher/StartDaemonCommand.php
@@ -0,0 +1,51 @@
+<?php
+namespace Jaggy\Watcher;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Starts the watcher daemon.
+ *
+ * @uses        \Symfony\Component\Console\Command\Command
+ * @uses        \Symfony\Component\Console\Input\InputInterface
+ * @uses        \Symfony\Component\Console\Output\OutputInterface
+ *
+ * @package     Jaggy\Watcher
+ * @author      Jaggy Gauran <jaggygauran@gmail.com>
+ * @version     Release: 0.1.0
+ * @link        https://github.com/
+ * @license     http://www.wtfpl.net/ Do What The Fuck You Want To Public License
+ * @since       Class available since Release 1.1.0
+ */
+class StartDaemonCommand extends Command
+{
+
+    /**
+     * Configure the command
+     *
+     * @access public
+     * @return void
+     */
+    public function configure()
+    {
+        $this->setName('start')
+             ->setDescription('Start the watcher daemon');
+    }
+
+
+    /**
+     * Execute the command
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @access public
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('test');
+    }
+}
