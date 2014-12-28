485df025f7011fae609a64bfa23eaa3b5290ed03
Jaggy Gauran <jaggygauran@gmail.com>
2014-12-28 11:32:23 +0800

    inject sessiins

--- src/Jaggy/Watcher/Monitor.php
+++ src/Jaggy/Watcher/Monitor.php
@@ -2,16 +2,32 @@
 namespace Jaggy\Watcher;
 
 /**
- * Monitor
+ * Monitors the sessions to see if a new user connects.
+ *
+ * @property    \Jaggy\Watcher\Session session
+ *
+ * @uses        \Jaggy\Watcher\Session
  *
  * @package     Jaggy\Watcher
  * @author      Jaggy Gauran <jaggygauran@gmail.com>
  * @version     Release: 0.1.0
- * @link        https://github.com/
+ * @link        https://github.com/jaggyspaghetti/ssh-watcher
  * @license     http://www.wtfpl.net/ Do What The Fuck You Want To Public License
  * @since       Class available since Release 1.1.0
  */
 class Monitor
 {
 
+
+    /**
+     * Inject the session to monitor.
+     *
+     * @param  \Jaggy\Watcher\Session $session
+     *
+     * @access public
+     */
+    public function __construct(Session $session)
+    {
+
+    }
 }
--- tests/MonitorTest.php
+++ tests/MonitorTest.php
@@ -6,7 +6,7 @@ use Jaggy\Watcher\Monitor;
  *
  * @author      Jaggy Gauran <jaggygauran@gmail.com>
  * @version     Release: 0.1.0
- * @link        https://github.com/
+ * @link        https://github.com/jaggyspaghetti/ssh-watcher
  * @license     http://www.wtfpl.net/ Do What The Fuck You Want To Public License
  * @since       Class available since Release 1.1.0
  */
@@ -22,7 +22,8 @@ class MonitorTest extends PHPUnit_Framework_TestCase
      */
     public function it_is_initializable()
     {
-        $monitor = new Monitor();
+        $session = Mockery::mock('Jaggy\Watcher\Session');
+        $monitor = new Monitor($session);
 
         $this->assertInstanceOf('Jaggy\Watcher\Monitor', $monitor);
     }
